1f1779dff8db41a432042ee2af328bc139cc6cea
Archashi Kamal <147809376+archashikamal@users.noreply.github.com>
2025-11-25 19:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ st.markdown("""
 # -----------------------------------------
 # OPENAI
 # -----------------------------------------
-client = OpenAI()
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # -----------------------------------------
 # LOAD/SAVE PROMPTS
